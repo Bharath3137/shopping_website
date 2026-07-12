301dba49f1d70efb2a813aac8b63ee0cc725c297
Bharath <bharath357890@gmail.com>
2026-07-12 23:28:01 +0530

    Build terminal shopping website with cart and checkout

--- main.py
+++ main.py
@@ -1,35 +1,64 @@
 products = {
-    1:"laptop",
-    2:"mobile"
+    1:{"name":"laptop",
+       "price":90000,
+      },
+    2:{"name":"mobile",
+       "price":70000},
     }
 
 cart = {}
 
 def view_products():
     for product_id,product in products.items():
-       print(product_id,product)
+       print(f"ID: {product_id}\nProduct Name: {product['name']}\nPrice: ₹{product['price']}")
 
 def search_products(search_name):
-   for product_id,product_name in products.items():
-         if search_name.lower() == product_name.lower():
-             return product_id,product_name
+   for product_id,product in products.items():
+         if search_name.lower() == product["name"].lower():
+             return product_id,product
    return "not found"
 
 def add_to_cart(product_id):
     if product_id not in products:
          return "invalid product_id"
     cart[product_id] = cart.get(product_id,0)+1
+    return "product added successfuly"
 
 def view_cart():
     if not cart:
         print("cart is empty")
         return
     for product_id,quantity in cart.items():
-        print(products[product_id],quantity)
-         
-print("1.view menu\n2.search products\n3.Add products to cart\n4View cart.\n5.Exit")
+        print(products[product_id]["name"],quantity)
+
+def remove_item_from_cart(product_id):
+        if product_id not in cart:
+             return "product not in cart"
+        cart[product_id]-=1
+        if cart[product_id]==0:
+             del cart[product_id]
+        return "sucessfully removed "
+def checkout():
+    if not cart:
+        print("empty cart")
+        return
+    total_amount=0
+    for product_id,quantity in cart.items():
+         name = products[product_id]["name"]
+         price = products[product_id]["price"]
+
+         subtotal = price * quantity
+         total_amount += subtotal
+
+         print(f"{name} x {quantity} = ₹{subtotal}")
+    print(f"Total Bill{total_amount}")
+    print("thankyou")
+    cart.clear()
+
+      
+
 while True:
-   print("1.view products\n2.search products\n3.Add products to cart\n4View cart.\n5.Exit")
+   print("1.view menu\n2.search products\n3.Add products to cart\n4.View cart.\n5.remove item from cart\n6.checkout\n10.Exit")
    choice=int(input("enter your choice"))
    if choice == 1:
       view_products()
@@ -41,10 +70,14 @@ while True:
        print(add_to_cart(product_id))
    elif choice == 4:
        view_cart()
-   elif choice == 5:
+   elif choice==5:
+       product_id=int(input("enter the id of product"))
+       print(remove_item_from_cart(product_id))
+   elif choice==6:
+       checkout()
+   elif choice == 10:
        print("thankyou")
        break
    else:
       print("please enter a valid input")
 
-    
